e84570d0dd88e74bc60eac63caeacb2f5fd6e5f0
dashakozachenko <dkozacenko4@gmail.com>
2021-12-10 15:42:43 +0200

    file changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $('.slider__slick').slick({
 dots:true
 });
+
+$('.slider__about').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    
+    responsive: [
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          
+        }
+      }
+    ]
+
+});
